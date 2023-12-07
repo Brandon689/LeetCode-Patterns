1a22b9594aaf2e2b4267015415c243468327b4ef
Brandon <brandon139@protonmail.com>
2023-12-08 00:49:56 +1100

    poc

---
+++ Sql.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Patterns
+{
+    public class Sql
+    {
+        public static void AddStock(SQLiteConnection db, Solution solution)
+        {
+            db.Insert(solution);
+        }
+    }
+}
